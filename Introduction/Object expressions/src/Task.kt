4b5d35f69cdbfe766d94e156bbe82e86bb4dd8c7
Rene Argento <rene.argento@gmail.com>
2018-09-21 22:56:14 +0200

    Added koans 1 to 15

--- Introduction/Object expressions/src/Task.kt
+++ Introduction/Object expressions/src/Task.kt
@@ -2,6 +2,8 @@ import java.util.*
 
 fun getList(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, object {})
+    Collections.sort(arrayList, object: Comparator<Int> {
+        override fun compare(num1: Int, num2: Int) = num2 - num1
+    })
     return arrayList
 }
